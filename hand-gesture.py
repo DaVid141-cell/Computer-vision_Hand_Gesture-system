dd05b7ee242acad87f50d7c4830203d3bb640a67
DaVid Justin <chaosdudeserad@gmail.com>
2025-12-24 03:37:38 +0800

    Initial commit

---
+++ hand-gesture.py
@@ -0,0 +1,23 @@
+import cv2
+import mediapipe as mp
+print("MediaPipe file:", mp.__file__)
+print("Dir:", dir(mp))
+
+
+mp_drawing = mp.solutions.drawing_utils
+mp_drawing_styles = mp.solutions.drawing_styles
+mphands = mp.solutions.hands
+
+cap = cv2.VideoCapture(0)
+hands = mphands.Hands()
+while True:
+    data, image = cap.read()
+
+    image = cv2.cvtColor(cv2.flip(image,1), cv2.COLOR_RGB2BGR)
+    result = hands.process(image)
+    image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
+    if result.multi_hand_landmarks:
+        for hand_landmarks in result.multi_hand_landmarks:
+            mp_drawing.draw_landmarks(image, hand_landmarks, mphands.HAND_CONNECTIONS)
+    cv2.imshow('Handtracker', image)
+    cv2.waitKey(1)
