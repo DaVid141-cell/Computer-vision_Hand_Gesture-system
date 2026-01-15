2de23ace874256c26ea349af4b38e1d43c970b87
DaVid Justin <chaosdudeserad@gmail.com>
2026-01-15 17:41:05 +0800

    Enhance data collection script to improve user interaction by updating text positioning and adding new gesture label input functionality

--- collect-data.py
+++ collect-data.py
@@ -35,6 +35,7 @@ with open("gesture-data.csv", "a", newline="") as f:
         image = cv2.cvtColor(cv2.flip(image,1), cv2.COLOR_BGR2RGB)
         results = hands.process(image)
         image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+        key = cv2.waitKey(1) & 0xFF
 
         if results.multi_hand_landmarks:
             for idx, hand_landmarks in enumerate(results.multi_hand_landmarks):
@@ -61,21 +62,31 @@ with open("gesture-data.csv", "a", newline="") as f:
                 r, l, _ = image.shape
                 x = int(hand_landmarks.landmark[0].x * r)
                 y = int(hand_landmarks.landmark[0].y * l)
+                
+                # text posistioning 
+                h, w, _ = image.shape
 
                 cv2.putText(image, display_text, (x - 40, y - 20),
                             cv2.FONT_HERSHEY_SIMPLEX, 1.5, (0, 255, 0), 3)
                 
-                cv2.putText(image, "Press S to save", (10, 40), 
-                        cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2)
+                cv2.putText(image, "Press S to save", (10, h - 60), 
+                            cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 1)
                 
-            if cv2.waitKey(1) & 0xFF == ord('s'):
+                cv2.putText(image, "Press C to create new", (10, h - 20),
+                            cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 1)
+            
+                
+            if key == ord('s'):
                 write.writerow(landmarks + [label])
-                print(f"Saved {label}")
-
-        cv2.imshow("Collect Data", image)
+                print(f"Saved {label} ({hand_label})")
+            elif key == ord('c'):
+                label = input("Enter New Gesture label: ")
+                print("Swicthed to label: {label}")
         
-        if cv2.waitKey(1) & 0xFF == 27:
+        elif key == 27:
             break
+        
+        cv2.imshow("Collect Data", image)
 
 cap.release()
 cv2.destroyAllWindows()
